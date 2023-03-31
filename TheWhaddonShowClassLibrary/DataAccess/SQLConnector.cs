f39fa9691abfcadf4aca016af8afc6a30d9f0922
Mark Carter <magcarter@hotmail.co.uk>
2023-03-31 09:16:18 +0100

    SQLConnector setup started

---
+++ TheWhaddonShowClassLibrary/DataAccess/SQLConnector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWhaddonShowClassLibrary.Models;
+
+namespace TheWhaddonShowClassLibrary.DataAccess
+{
+    internal class SQLConnector : IDataAccessServer
+    {
+        private static string CnnString = "connection sttring to be sort out";
+
+        public List<Part> getPartsByPerson(int personID, bool activeOnly = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Part> getPartsByScriptItem(int? scriptItemID = null, bool activeOnly = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> getPersonsByScriptItem(int? scriptItemID = null, bool activeOnly = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ScriptItem> getScriptItems(int? scriptItemID = null, bool latestOnly = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ScriptItem> getScriptItemsByPerson(int personID, bool latestOnly = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void savePart(Part part)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void savePerson(Part part)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void saveScriptItem(ScriptItem scriptItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
