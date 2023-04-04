f8d985ac4137fe96ce68fc1630c75fb30a0611c8
Mark Carter <magcarter@hotmail.co.uk>
2023-04-04 17:37:04 +0100

    Creation of WhaddonShowConsoleUI

---
+++ TheWhaddonShowConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine(GetConnectionString());
+
+
+static string GetConnectionString(string connectionStringName="Default")
+{
+    string output;
+    
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory()) .AddJsonFile("appsettings.json");
+
+    var config = builder.Build();
+
+    output = config.GetConnectionString(connectionStringName) ?? String.Empty;
+
+    return output;
+}
