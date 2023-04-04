44e656e4948d6085cc5149e05cbd6d24288ccad1
Mark Carter <magcarter@hotmail.co.uk>
2023-04-04 17:38:52 +0100

    Creation of spLoadChangesFromServer

---
+++ TheWhaddonShowDB/dbo/StoredProcedures/spLoadChangesFromServer.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE [dbo].[spLoadChangesFromServer]
+	@ObjectType varchar(255),
+	@LastSyncDate DateTime
+AS
+
+IF @ObjectType = 'PartUpdate'
+BEGIN
+	RETURN (SELECT [Id], [UpdatedLocally], [UpdatedOnServer], [ConflictID], [IsActive], [Name], [PersonID], [Tags]
+			FROM dbo.PartLog
+			WHERE UpdatedOnServer > @LastSyncDate
+			FOR JSON AUTO);
+END
+
+IF @ObjectType = 'PersonUpdate'
+BEGIN
+	RETURN (SELECT [Id], [UpdatedLocally], [UpdatedOnServer], [ConflictID], [IsActive], [FirstName], [LastName], [Email], [PictureRef], [IsActor], [IsSinger], [IsWriter], [isBand], [IsTechnical], [Tags]
+			FROM dbo.PersonLog
+			WHERE UpdatedOnServer > @LastSyncDate
+			FOR JSON AUTO);
+END
+
+IF @ObjectType = 'ScriptItemUpdate'
+BEGIN
+	RETURN (SELECT [Id], [UpdatedLocally], [UpdatedOnServer], [ConflictID], [IsActive], [ParentID], [OrderNo], [Type], [Parts], [Tags]
+			FROM dbo.ScriptItemLog
+			WHERE UpdatedOnServer > @LastSyncDate
+			FOR JSON AUTO);
+END
+
+
+
+
+
+
