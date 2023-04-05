38248306b2913489c33cfb120092009ff357ea96
Mark Carter <magcarter@hotmail.co.uk>
2023-04-05 15:48:21 +0100

    Creation og First DB Tests

---
+++ TheWhaddonShowDBTests/spLoadChangesFromServerTest.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+
+namespace TheWhaddonShowDBTests
+{
+    [TestClass()]
+    public class spLoadChangesFromServerTest : SqlDatabaseTestClass
+    {
+
+        public spLoadChangesFromServerTest()
+        {
+            InitializeComponent();
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            base.InitializeTest();
+        }
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            base.CleanupTest();
+        }
+
+        #region Designer support code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction dbo_spLoadChangesFromServerTest_TestAction;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(spLoadChangesFromServerTest));
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.InconclusiveCondition inconclusiveCondition1;
+            this.dbo_spLoadChangesFromServerTestData = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestActions();
+            dbo_spLoadChangesFromServerTest_TestAction = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction();
+            inconclusiveCondition1 = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.InconclusiveCondition();
+            // 
+            // dbo_spLoadChangesFromServerTestData
+            // 
+            this.dbo_spLoadChangesFromServerTestData.PosttestAction = null;
+            this.dbo_spLoadChangesFromServerTestData.PretestAction = null;
+            this.dbo_spLoadChangesFromServerTestData.TestAction = dbo_spLoadChangesFromServerTest_TestAction;
+            // 
+            // dbo_spLoadChangesFromServerTest_TestAction
+            // 
+            dbo_spLoadChangesFromServerTest_TestAction.Conditions.Add(inconclusiveCondition1);
+            resources.ApplyResources(dbo_spLoadChangesFromServerTest_TestAction, "dbo_spLoadChangesFromServerTest_TestAction");
+            // 
+            // inconclusiveCondition1
+            // 
+            inconclusiveCondition1.Enabled = true;
+            inconclusiveCondition1.Name = "inconclusiveCondition1";
+        }
+
+        #endregion
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        #endregion
+
+        [TestMethod()]
+        public void dbo_spLoadChangesFromServerTest()
+        {
+            SqlDatabaseTestActions testActions = this.dbo_spLoadChangesFromServerTestData;
+            // Execute the pre-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PretestAction != null), "Executing pre-test script...");
+            SqlExecutionResult[] pretestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PretestAction);
+            try
+            {
+                // Execute the test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.TestAction != null), "Executing test script...");
+                SqlExecutionResult[] testResults = TestService.Execute(this.ExecutionContext, this.PrivilegedContext, testActions.TestAction);
+            }
+            finally
+            {
+                // Execute the post-test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.PosttestAction != null), "Executing post-test script...");
+                SqlExecutionResult[] posttestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PosttestAction);
+            }
+        }
+        private SqlDatabaseTestActions dbo_spLoadChangesFromServerTestData;
+    }
+}
