b18044074157b9ebdf846c50700ee8195321f60b
Mark Carter <magcarter@hotmail.co.uk>
2023-05-09 17:25:45 +0100

    Creation of TheWhaddonShowAPI prior to testing.

---
+++ TheWhaddonShowAPI/Controllers/PartsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyClassLibrary.LocalServerMethods;
+using Newtonsoft.Json;
+using TheWhaddonShowClassLibrary.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TheWhaddonShowAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartsController : ControllerBase
+    {
+        private readonly IServerDataAccess _serverDataAccess;
+
+        public PartsController (IServerDataAccess serverDataAccess)
+        {
+            _serverDataAccess = serverDataAccess;
+        }
+
+        // GET: api/parts/
+        [HttpGet("{ids}")]
+        public string Get(string ids)
+        {
+            List<Guid> idsList = new List<Guid>();
+            idsList = JsonConvert.DeserializeObject<List<Guid>>(ids) ?? new List<Guid>();
+
+            List<PartUpdate> partUpdates = _serverDataAccess.GetFromServer<PartUpdate>(idsList);
+            
+            string output = JsonConvert.SerializeObject(partUpdates);
+
+            return output;
+        }
+
+        // GET api/parts/changes/2023-05-09T10:23:56.024Z
+        [HttpGet("changes/{lastSyncDate}")]
+        public string GetChanges(DateTime lastSyncDate)
+        {
+            (List<PartUpdate> partUpdates, DateTime lastUpdatedOnServer) = _serverDataAccess.GetChangesFromServer<PartUpdate>(lastSyncDate);
+
+            string output =  JsonConvert.SerializeObject(partUpdates);
+
+            return output;
+        }
+
+        // POST api/parts/
+        [HttpPost("{updates}")]
+        public void Post([FromBody] string updates)
+        { 
+            List<PartUpdate> partUpdates = JsonConvert.DeserializeObject<List<PartUpdate>>(updates) ?? new List<PartUpdate>();
+
+            _serverDataAccess.SaveToServer(partUpdates);
+        }
+
+        // POST api/parts/conflicts
+        [HttpPost("conflicts/{conflicts}")]
+        public void PostConflicts([FromBody] string conflicts)
+        {
+            List<Conflict> partConflicts = JsonConvert.DeserializeObject<List<Conflict>>(conflicts) ?? new List<Conflict>();
+
+            _serverDataAccess.SaveConflictIdsToServer<PartUpdate>(partConflicts);
+        }
+
+        // DELETE api/parts/
+        [HttpDelete("{updates}")]
+        public void Delete([FromBody] string updates)
+        {
+
+            List<PartUpdate> partUpdates = JsonConvert.DeserializeObject<List<PartUpdate>>(updates) ?? new List<PartUpdate>();
+
+            _serverDataAccess.DeleteFromServer(partUpdates);
+        }
+    }
+}
---
+++ TheWhaddonShowAPI/Program.cs
@@ -0,0 +1,41 @@
+using MyClassLibrary.DataAccessMethods;
+using MyClassLibrary.LocalServerMethods;
+using MyClassLibrary.Tests.LocalServerMethods;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+IConfiguration config;
+var configBuilder = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json");
+config = configBuilder.Build();
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+builder.Services.AddSingleton<IServerDataAccess, ServerSQLConnector>();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
