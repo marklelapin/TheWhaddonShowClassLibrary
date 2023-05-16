51eecf7e44cd4c820eb5b64e1c9ab9b7771b8b9d
Mark Carter <magcarter@hotmail.co.uk>
2023-05-16 17:51:49 +0100

    Added Basic Authentication

--- TheWhaddonShowAPI/Program.cs
+++ TheWhaddonShowAPI/Program.cs
@@ -1,26 +1,111 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MyClassLibrary.DataAccessMethods;
 using MyClassLibrary.LocalServerMethods;
 using MyClassLibrary.Tests.LocalServerMethods;
-
+using System.Reflection;
+using System.Text;
+using TheWhaddonShowClassLibrary.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-IConfiguration config;
-var configBuilder = new ConfigurationBuilder()
-                   .SetBasePath(Directory.GetCurrentDirectory())
-                   .AddJsonFile("appsettings.json");
-config = configBuilder.Build();
+//IConfiguration config;
+//var configBuilder = new ConfigurationBuilder()
+//                   .SetBasePath(Directory.GetCurrentDirectory())
+//                   .AddJsonFile("appsettings.json");
+//config = configBuilder.Build();
 
 
 // Add services to the container.
 
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+            ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+        };
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opts =>
+{
+    var title = "The Whaddon Show API";
+    var description = "Provides data access to The Whaddon Show's central database.";
+    var contact = new OpenApiContact()
+    {
+        Name = "Mark Carter",
+        Email = "magcarter@hotmail.co.uk"
+        //TODO - add in home page
+    };
+
+    opts.SwaggerDoc("v1 (deprecated)", new OpenApiInfo()
+    {
+        Version = "v1",
+        Title = title,
+        Description = description,
+        Contact = contact
+    });
+    opts.SwaggerDoc("v2", new OpenApiInfo()
+    {
+        Version = "v2",
+        Title = title,
+        Description = description,
+        Contact = contact
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    opts.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFile));
+});
+builder.Services.AddApiVersioning(opts =>
+{
+    opts.AssumeDefaultVersionWhenUnspecified = true;
+    opts.DefaultApiVersion = new(2, 0);
+    opts.ReportApiVersions = true;
+});
+
+builder.Services.AddVersionedApiExplorer(opts =>
+{
+    opts.GroupNameFormat = "'v'VVV";
+    opts.SubstituteApiVersionInUrl = true;
+});
+
+
 builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
 builder.Services.AddSingleton<IServerDataAccess, ServerSQLConnector>();
+
+
+
+
+
+
+
+
+
 
 var app = builder.Build();
 
@@ -29,11 +114,18 @@ var app = builder.Build();
 //if (app.Environment.IsDevelopment())
 //{
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(opts =>
+    {
+        opts.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+        opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        opts.RoutePrefix = string.Empty;
+        
+    });
 //}
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
