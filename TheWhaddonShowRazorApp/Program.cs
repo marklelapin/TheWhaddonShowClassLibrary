a8d6aa9440ec3388ec4df3d9bfa0ee3ec9fd1db8
Mark Carter <magcarter@hotmail.co.uk>
2023-06-14 15:29:02 +0100

    Added Authentication to Razor App and created Builder Extensions to use in future.

--- TheWhaddonShowRazorApp/Program.cs
+++ TheWhaddonShowRazorApp/Program.cs
@@ -1,45 +1,68 @@
+using Microsoft.Identity.Web.UI;
 using MyClassLibrary.DataAccessMethods;
 using MyClassLibrary.Extensions;
 using MyClassLibrary.LocalServerMethods;
 using MyClassLibrary.LocalServerMethods.Interfaces;
 using MyClassLibrary.LocalServerMethods.Models;
-using TheWhaddonShowClassLibrary.Models;
+
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.ConfigureAuthentication_AzureAdB2C();
+builder.ConfigureWebAuthentication_AzureAdB2C();
 
-builder.ByPassAuthenticationIfInDevelopment();
+builder.RequireAuthorizationThroughoutAsFallbackPolicy();
 
-// Add services to the container.
-builder.Services.AddRazorPages();  //Do I need to add something more her for Azure AdB2C
+//builder.ByPassAuthenticationIfInDevelopment();   
 
+// Add Razor Pages and authorize access
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AllowAnonymousToPage("/Index");
+    options.Conventions.AllowAnonymousToPage("/Privacy");
+    options.Conventions.AllowAnonymousToFolder("/Script");
+
+});
+
+
+
+//Add services
+builder.Services.AddTransient<ILocalServerModelUpdate, LocalServerModelUpdate>();
+builder.Services.AddTransient(typeof(ILocalServerModel<>),typeof(LocalServerModel<>));
 builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
 builder.Services.AddScoped(typeof(ILocalDataAccess<>),typeof(LocalSQLConnector<>));
 builder.Services.AddSingleton(typeof(IServerDataAccess<>),typeof(ServerSQLConnector<>));
-builder.Services.AddSingleton(typeof(ILocalServerEngine<>), typeof(LocalServerEngine<>));
-builder.Services.AddSingleton(typeof(ILocalServerModel<>), typeof(LocalServerModel<>));
-builder.Services.AddSingleton(typeof(ILocalServerModelFactory<,>), typeof(LocalServerModelFactory<,>));
+builder.Services.AddScoped(typeof(ILocalServerEngine<>), typeof(LocalServerEngine<>));
+builder.Services.AddScoped(typeof(ILocalServerModelFactory<,>), typeof(LocalServerModelFactory<,>));
 
 
 var app = builder.Build();
-// Configure the HTTP request pipeline.
+
+
+//Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseStatusCodePagesWithRedirects("/Error/{0}");
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseCookiePolicy();
+
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
+
+
 app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
