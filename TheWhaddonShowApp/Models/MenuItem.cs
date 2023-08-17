b7fb15e85762da5c24a7c0d7be276901ff9ebd12
Mark Carter <magcarter@hotmail.co.uk>
2023-08-17 14:17:56 +0100

    partial views created for sidebar menu items but asp-actions not working and failing to open sub menus

---
+++ TheWhaddonShowApp/Models/MenuItem.cs
@@ -0,0 +1,35 @@
+﻿namespace TheWhaddonShowApp.Models
+{
+	public class MenuItem
+	{
+		public string Text { get; set; }
+
+		public string? Icon { get; set; }
+
+		public string? IconLabel { get; set; }
+
+		public string Controller { get; set; }
+
+		public string? Action { get; set; }
+
+		public List<MenuItem>? SubItems { get; set; }
+
+		public MenuItem(string text, string controller, string? action)
+		{
+			Text = text;
+			Controller = controller;
+			Action = action;
+		}
+
+
+		public MenuItem(string text, string controller, string? action, string? icon, List<MenuItem>? subItems = null)
+		{
+			Text = text;
+			Controller = controller;
+			Action = action;
+			Icon = icon;
+			SubItems = subItems;
+		}
+
+	}
+}
