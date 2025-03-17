42efa8f24cb70ff1409452a123a1238fd99ff312
Mark Carter <magcarter@hotmail.co.uk>
2025-03-17 22:15:05 +0000

    Settings and CowboyShoutOut added to Library

---
+++ TheWhaddonShowClassLibrary/Models/Settings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWhaddonShowClassLibrary.Models
+{
+
+
+
+    public class Settings
+    {
+
+        public Settings(Guid showId,CowboyShoutOut cowboyShoutOut) {
+            CurrentShowId = showId;
+            CowboyShoutOut = cowboyShoutOut;
+        }
+
+        public Guid CurrentShowId { get; set; }
+
+        public CowboyShoutOut CowboyShoutOut {get;set;}
+
+
+
+    }
+
+    public class CowboyShoutOut
+    {
+        bool ShowDaysTillOpeningNight { get; set; } = true;
+        bool ShowCastingStatistics { get; set; } = true;
+        DateTime? NextRehearsalDate { get; set; } = null;
+        string? AdditionalMessage { get; set; } = null;
+    }
+}
