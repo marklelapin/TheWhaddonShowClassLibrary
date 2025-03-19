1b08c974aeb26fd80eefa43873cc33c9e6419b9e
Mark Carter <magcarter@hotmail.co.uk>
2025-03-19 22:17:39 +0000

    CowboyShoutOut and ShowDates added

--- TheWhaddonShowClassLibrary/Models/CowboyShoutOut.cs
+++ TheWhaddonShowClassLibrary/Models/CowboyShoutOut.cs
@@ -6,30 +6,12 @@ using System.Threading.Tasks;
 
 namespace TheWhaddonShowClassLibrary.Models
 {
-
-
-
-    public class Settings
-    {
-
-        public Settings(Guid showId,CowboyShoutOut cowboyShoutOut) {
-            CurrentShowId = showId;
-            CowboyShoutOut = cowboyShoutOut;
-        }
-
-        public Guid CurrentShowId { get; set; }
-
-        public CowboyShoutOut CowboyShoutOut {get;set;}
-
-
-
-    }
-
     public class CowboyShoutOut
     {
         bool ShowDaysTillOpeningNight { get; set; } = true;
         bool ShowCastingStatistics { get; set; } = true;
         DateTime? NextRehearsalDate { get; set; } = null;
         string? AdditionalMessage { get; set; } = null;
+
     }
 }
---
+++ TheWhaddonShowClassLibrary/Models/ShowDates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWhaddonShowClassLibrary.Models
+{
+    public class ShowDates
+    {
+        public DateTime? OpeningNight { get; set; } 
+
+        public DateTime? LastNight { get; set; }
+
+    }
+}
